a55b6afa0c27b84a3bed33da605b98c96379f2a6
Ciolocoi Laurențiu <lciolocoi@outlook.com>
2024-10-30 11:12:39 +0200

    30.10.2024

--- Ex12Compositionoffunctions.py
+++ Ex12Compositionoffunctions.py
@@ -2,8 +2,8 @@
 def f(x):
     return x+1
 def comp_f(f, x, n):
-    if n == 0:
+    if n == 1:
         return f(x)
     else:
         return f(comp_f(f, x, n-1))
-print(comp_f(f,2,4))
+print(comp_f(f,1,2))
--- Ex8Themy_reversefunction.py
+++ Ex8Themy_reversefunction.py
@@ -2,5 +2,5 @@ def my_reverse(s, i=0):
     if len(s) == 0 :
         return s
     else:
-        return my_reverse(s[1:])+s[0];
+        return my_reverse(s[1:])+s[0]
 print(my_reverse("abcde"))
